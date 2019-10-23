fc7042f6d773278950f16664806c2202d3583364
Imam <nawawu932@gmail.com>
2019-10-23 12:39:18 +0700

    Setup Sidebar and TopNav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
+  <div id="app-container">
+  <top-nav/>
+  <sidebar/>
+  
     <router-view/>
+  </div>
 </template>
 <script>
+import Sidebar from '@/containers/Sidebar'
+import TopNav from '@/containers/TopNav'
+
 export default {
-  name: 'Home',
+  data () {
+  return { show: false }  
+  },
+  components: {
+    TopNav,
+    Sidebar
+  }
 };
 </script>
